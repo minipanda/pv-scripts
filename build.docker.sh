da5d09b5869fbfd0fb5549aced54c2d1d70c033a
Alexander Sack <asac@pantacor.com>
2018-04-23 12:02:26 +0200

    pass MAKEFLAGS into build.docker.sh to allow for tweaking things like -jX in docker build as well

--- build.docker.sh
+++ build.docker.sh
@@ -17,6 +17,6 @@ usercontainer_tag=pv-build:pv-dev-`id -u`
 sh -c "cd $dir; docker build --tag=pantavisor/pv-build -f Dockerfile.build-base ."
 sh -c "cd $dir; docker build --tag=$usercontainer_tag -f Dockerfile.build ."
 
-docker run -v$PWD:/pwd -t --user `id -u` --env-file=<(env | grep -v PATH | grep -v LD_LIBR | grep -v PKG_ | grep -v PYTHON) --rm $usercontainer_tag $@
+docker run -e MAKEFLAGS=$MAKEFLAGS -v$PWD:/pwd -t --user `id -u` --env-file=<(env | grep -v PATH | grep -v LD_LIBR | grep -v PKG_ | grep -v PYTHON) --rm $usercontainer_tag $@
 
 
