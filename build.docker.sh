72397ff72c534ab2096cd31efb1e80e352c1f587
Alexander Sack <asac@pantacor.com>
2018-05-29 13:37:59 +0000

    make build.docker.sh smarter by using proper -w working directory

--- build.docker.sh
+++ build.docker.sh
@@ -17,6 +17,21 @@ usercontainer_tag=pv-build:pv-dev-`id -u`
 sh -c "cd $dir; docker build --tag=pantavisor/pv-build -f Dockerfile.build-base ."
 sh -c "cd $dir; docker build --tag=$usercontainer_tag -f Dockerfile.build ."
 
-docker run -e MAKEFLAGS=$MAKEFLAGS -v$PWD:/pwd -t --user `id -u` --env-file=<(env | grep -v PATH | grep -v LD_LIBR | grep -v PKG_ | grep -v PYTHON) --rm $usercontainer_tag $@
 
+pvr_merge_src_abs=
+
+if [ -d "$PVR_MERGE_SRC" ]; then
+	pvr_merge_src_abs=`sh -c "cd $PVR_MERGE_SRC; pwd"`
+fi
+
+docker run \
+	-e MAKEFLAGS=$MAKEFLAGS \
+	-v$PWD:$PWD \
+	-v$pvr_merge_src_abs:$pvr_merge_src_abs \
+	-w$PWD \
+	--user `id -u` \
+	--env-file=<(env | grep -v PATH | grep -v LD_LIBR | grep -v PKG_ | grep -v PYTHON) \
+	-t --rm \
+	$usercontainer_tag \
+	$@
 
